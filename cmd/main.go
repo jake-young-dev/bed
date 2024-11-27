8fcf0fecdb90c7e8f993b4a3ea3c27971144b0db
unknown <17075259+jake-young-dev@users.noreply.github.com>
2024-11-27 13:42:51 -0500

    testing passed

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
 
 	"github.com/jake-young-dev/bed/cron"
@@ -32,16 +33,15 @@ func main() {
 
 	log.Println("starting backup cron")
 	backup := cron.NewCronHandler()
-	backup.TakeBackup()
-	// backup.Run()
+	backup.Run()
 
-	// //wait for interupt
-	// quit := make(chan os.Signal, 1)
-	// signal.Notify(quit, os.Interrupt)
-	// <-quit
+	//wait for interupt
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
 
-	// log.Println("stopping backup cron")
-	// backup.Stop()
+	log.Println("stopping backup cron")
+	backup.Stop()
 }
 
 // validates the existence of necessary environment variables required to run the cron
